d5802e15239dea69681f6ea35862bf0501f1470b
davidmoates <david.m.moates@gmail.com>
2020-09-08 21:37:43 -0500

    finished all

--- src/15_classes.py
+++ src/15_classes.py
@@ -3,7 +3,7 @@
 
 # YOUR CODE HERE
 class LatLon:
-	def __init__(self, lat,lon):
+	def __init__(self, lat, lon):
 		self.lat = lat
 		self.lon = lon
 
@@ -12,19 +12,26 @@ class LatLon:
 
 # YOUR CODE HERE
 class Waypoint(LatLon):
-	def __init__(self, name, **kwargs):
+	def __init__(self, name, lat, lon):
 		self.name = name
-		super().__init__(**kwargs)
+		super().__init__(lat, lon)
+
+	def __str__(self):
+		return 'WayPoint(name: '+self.name+', latitude: '+str(self.lat) + ', longitude: '+str(self.lon)+')'
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
 class Geocache(Waypoint):
-	def __init__(self, difficulty, size, **kwargs):
+	def __init__(self, name, difficulty, size, lat, lon):
 		self.difficulty = difficulty
 		self.size = size
-		super().__init__(**kwargs)
+		super().__init__(name, lat, lon)
+
+	def __str__(self):
+		return 'Geocache(name: '+self.name+', difficulty: '+self.difficulty+', size: '+self.size+', latitude: '+str(self.lat) + ', longitude: '+str(self.lon)+')'
+
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
@@ -34,11 +41,13 @@ print(f'"{waypoint.name}", {waypoint.lat}, {waypoint.lon}')
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
+
 print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
+geocache = Geocache("Newberry Views", "diff 1.5", "size 2", 44.052137, -121.41556).__str__()
 
 # Print it--also make this print more nicely
 print(geocache)
